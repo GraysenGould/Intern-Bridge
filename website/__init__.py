c555ed0e7e508383be392147535c10197dc1ebcc
Graysen Gould <gouldgraysen@gmail.com>
2024-11-28 20:47:08 -0600

    added some html files

--- website/__init__.py
+++ website/__init__.py
@@ -5,6 +5,8 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+    app.config["SECRET_KEY"] = "idc"
+
     from .views import views
 
     app.register_blueprint(views, url_prefix = "/")
--- website/views.py
+++ website/views.py
@@ -1,7 +1,12 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, render_template, request
 
 views = Blueprint("views", __name__)
 
 @views.route("/", methods = ["POST", "GET"])
 def home():
-    return render_template("base.html")
+    return render_template("home.html")
+
+
+@views.route("/interview",methods = ["GET"])
+def interview():
+    return render_template("interview.html")
