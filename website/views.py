8c9770fcc41cdf3a16fe201370088f5b9f2f26cc
Graysen Gould <gouldgraysen@gmail.com>
2024-12-03 15:06:56 -0600

    two pages now transfer data

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint, render_template, request, redirect, url_for
+from flask import Flask, Blueprint, render_template, request, redirect, url_for, session
 
 views = Blueprint("views", __name__)
 
@@ -10,7 +10,8 @@ def home():
 @views.route("/interview",methods = ["GET", "POST"])
 def interview():
     if request.method == "POST":
-        print(request.form.get("jobDescriptionInput"))
+        session["jobDescriptionInput"] = request.form.get("jobDescriptionInput")
+        session["resumeInput"] = request.form.get("resumeInput")
         return redirect(url_for("views.interview_questions"))
         #print("Forms posted to backend")
     return render_template("interview.html")
@@ -18,4 +19,8 @@ def interview():
 
 @views.route("/interview-questions",methods = ["GET", "POST"])
 def interview_questions():
-    return render_template("interview-questions.html")
+    job_description = session.get("jobDescriptionInput")
+    resume_info = session.get("resumeInput")
+    print(job_description, resume_info)
+
+    return render_template("interview-questions.html",job_description = job_description, resume_info = resume_info)
