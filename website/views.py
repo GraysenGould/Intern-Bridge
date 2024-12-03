25d903de09c78ad014cf34a8bc971e1dcdd522a4
Graysen Gould <gouldgraysen@gmail.com>
2024-12-02 23:02:16 -0600

    added generated question page

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint, render_template, request
+from flask import Flask, Blueprint, render_template, request, redirect, url_for
 
 views = Blueprint("views", __name__)
 
@@ -10,5 +10,12 @@ def home():
 @views.route("/interview",methods = ["GET", "POST"])
 def interview():
     if request.method == "POST":
-        print("Forms posted to backend")
+        print(request.form.get("jobDescriptionInput"))
+        return redirect(url_for("views.interview_questions"))
+        #print("Forms posted to backend")
     return render_template("interview.html")
+
+
+@views.route("/interview-questions",methods = ["GET", "POST"])
+def interview_questions():
+    return render_template("interview-questions.html")
